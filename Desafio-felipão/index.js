8d38aeb0a41c22fccced2ddb6a35dcbe549e08b5
Victor Aliatti <Victorshay@hotmail.com>
2025-07-07 13:04:37 -0300

    Arrumado e pronto

--- Desafio-felipão/index.js
+++ Desafio-felipão/index.js
@@ -4,29 +4,29 @@ const readline = require('readline').createInterface({
     });
 readline.question('Digite o nome do herói: ', nome => {
     readline.question('Digite a quantidade de XP: ', xpInput => {
-        const xp = parent(xpInput);
+        const xp = parseInt(xpInput);
         let nivel = "";
 
         if (xp <= 1000){
-            nivel = ferro;
+            nivel = "ferro";
         }else if (xp <= 2000){
-            nivel = bronze;
+            nivel = "bronze";
         }else if (xp <= 5000){
-            nivel = prata;
+            nivel = "prata";
         }else if  (xp <= 7000){
-            nivel = Ouro;
+            nivel = "Ouro";
         }else if  (xp <= 8000){
-            nivel = Platina;
+            nivel = "Platina";
         }else if (xp <= 9000){
-            nivel = Acendente; 
+            nivel = "Ascendente"; 
         }else if (xp <= 10000){
-            nivel = Imoral;
+            nivel = "Imortal";
         }else {
-            nivel = Radiante
+            nivel = "Radiante";
         }      
     
-      console.log(`O Herói de nome ${nome} está no nível de ${xp}`);
+      console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
     
-    readline.close(); // fecha após tudo estar feito
+    readline.close();
   });
 });
