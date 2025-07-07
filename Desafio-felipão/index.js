ac3c98a89fcc3a2c7794903320bf9f2e20cfa563
Victor Aliatti <Victorshay@hotmail.com>
2025-07-07 12:58:22 -0300

    Teste

---
+++ Desafio-felipão/index.js
@@ -0,0 +1,32 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    });
+readline.question('Digite o nome do herói: ', nome => {
+    readline.question('Digite a quantidade de XP: ', xpInput => {
+        const xp = parent(xpInput);
+        let nivel = "";
+
+        if (xp <= 1000){
+            nivel = ferro;
+        }else if (xp <= 2000){
+            nivel = bronze;
+        }else if (xp <= 5000){
+            nivel = prata;
+        }else if  (xp <= 7000){
+            nivel = Ouro;
+        }else if  (xp <= 8000){
+            nivel = Platina;
+        }else if (xp <= 9000){
+            nivel = Acendente; 
+        }else if (xp <= 10000){
+            nivel = Imoral;
+        }else {
+            nivel = Radiante
+        }      
+    
+      console.log(`O Herói de nome ${nome} está no nível de ${xp}`);
+    
+    readline.close(); // fecha após tudo estar feito
+  });
+});
