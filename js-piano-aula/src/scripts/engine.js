3fdc873068f120e5684cac3726e5e6f1cf74c9b8
Victor Aliatti <Victorshay@hotmail.com>
2025-07-25 19:11:26 -0300

    keybord

--- js-piano-aula/src/scripts/engine.js
+++ js-piano-aula/src/scripts/engine.js
@@ -5,9 +5,20 @@ let audio = new Audio("src/tunes/a.wav");
 const playTune = (key) => {
     audio.src=`src/tunes/${key}.wav`
     audio.play();
+    
+    const clickkeyKey = document.querySelector (`[data-key= "${key}"]`)
+    clickkeyKey.classList.add("active")
+    setTimeout(()  => {
+        clickkeyKey.classList.remove("active");
+    }, 150);
 };
+
 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key);
     key.addEventListener("click", () =>  playTune(key.dataset.key));
 });
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key);
+} );
